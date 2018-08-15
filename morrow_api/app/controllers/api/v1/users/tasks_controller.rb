4313e0cbdb528ce18b6fe6a0a7799e26b72da357
Jude Lawson <lawsonjdutton@gmail.com>
2018-08-15 17:54:00 -0600

    Updates users/tasks#index action to use current user instead of class level methods

--- morrow_api/app/controllers/api/v1/users/tasks_controller.rb
+++ morrow_api/app/controllers/api/v1/users/tasks_controller.rb
@@ -1,9 +1,9 @@
 class Api::V1::Users::TasksController < Api::V1::ApplicationController
   before_action :require_id_match, only: [:index]
-  
+
   def index
     begin
-      user_tasks = User.find(params[:id]).tasks
+      user_tasks = current_user.tasks
       render json: user_tasks
     rescue => error
       render json: { error: error.to_s }, status: 404
