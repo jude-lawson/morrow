df3e38fa63f41fde234685e107c7cc986aa346e4
Jude Lawson <lawsonjdutton@gmail.com>
2018-08-15 16:47:09 -0600

    Extracts create_user method out of create user action

--- morrow_api/app/controllers/api/v1/users_controller.rb
+++ morrow_api/app/controllers/api/v1/users_controller.rb
@@ -1,9 +1,14 @@
 class Api::V1::UsersController < ApplicationController
   def create
-    user_info = JSON.parse(request.body.string)['info']
-    token = JWT.encode user_info['password'], ENV['APP_SECRET'], 'HS256'
-    user_info['token'] = token
-    user = User.create!(user_info)
+    user = create_user(request.body.string)
     render json: { token: user.token }
+  end
+
+  private
+
+  def create_user(payload)
+    info = JSON.parse(payload)['info']
+    info['token'] = create_token(info)
+    User.create!(info)
   end
 end
