7f4b5d1e1e363b9036eb07ea90f2e7e8d31c6f98
Jude Lawson <lawsonjdutton@gmail.com>
2018-08-15 16:42:12 -0600

    Updates user token creation to utilize algorithmic encoding

--- morrow_api/app/controllers/api/v1/users_controller.rb
+++ morrow_api/app/controllers/api/v1/users_controller.rb
@@ -1,7 +1,7 @@
 class Api::V1::UsersController < ApplicationController
   def create
     user_info = JSON.parse(request.body.string)['info']
-    token = JWT.encode user_info['password'], nil, 'none'
+    token = JWT.encode user_info['password'], ENV['APP_SECRET'], 'HS256'
     user_info['token'] = token
     user = User.create!(user_info)
     render json: { token: user.token }
