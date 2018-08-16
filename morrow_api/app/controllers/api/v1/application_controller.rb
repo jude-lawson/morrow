d8c3f60f1ecbc0127964983116074849f4c7283b
Jude Lawson <lawsonjdutton@gmail.com>
2018-08-15 21:53:36 -0600

    Updates ApplicationController methods for current_user and require_id_match to accommodate proper user case flow and fa iluers

--- morrow_api/app/controllers/api/v1/application_controller.rb
+++ morrow_api/app/controllers/api/v1/application_controller.rb
@@ -4,12 +4,17 @@ class Api::V1::ApplicationController < ActionController::API
   end
 
   def current_user
-    provided_token = request.headers['Authorization'].split(' ')[1]
-    User.find_by(token: provided_token)
+    auth = request.headers['Authorization']
+    if auth && !auth.nil?
+      provided_token = auth.split(' ')[1]
+      User.find_by(token: provided_token)
+    else
+      nil
+    end
   end
 
   def require_id_match
-    if current_user.id.to_s != params[:id]
+    if current_user.class != User || current_user.id.to_s != params[:id]
       render status: 403
     end
   end
--- morrow_api/spec/requests/api/v1/task_requests_spec.rb
+++ morrow_api/spec/requests/api/v1/task_requests_spec.rb
@@ -2,11 +2,16 @@ require 'rails_helper'
 
 RSpec.describe 'Task Requests' do
   before :each do
-    @user1 = User.create!(first_name: 'John', last_name: 'Watson', email: 'jwatson@bakerstreet.com', password: 'sherlocked') 
+    app_controller = Api::V1::ApplicationController.new
+    user1_data = { first_name: 'John', last_name: 'Watson', email: 'jwatson@bakerstreet.com', password: 'sherlocked' }
+    user1_data['token'] = app_controller.create_token(user1_data[:password])
+    @user1 = User.create!(user1_data) 
     @task1 = @user1.tasks.create!(title: 'Title 1', body: 'Task 1 content')
     @task2 = @user1.tasks.create!(title: 'Title 2', body: 'Task 2 content')
 
-    @user2 = User.create!(first_name: 'Sherlock', last_name: 'Holmes', email: 'sholmes@bakerstreet.moc', password: 'mycroftisannoying')
+    user2_data = { first_name: 'Sherlock', last_name: 'Holmes', email: 'sholmes@bakerstreet.moc', password: 'mycroftisannoying' }
+    user2_data['token'] = app_controller.create_token(user2_data[:password])
+    @user2 = User.create!(user2_data)
     @user2.tasks.create!(title: 'Title 3', body: 'Task 3 content')
   end
 
@@ -19,8 +24,17 @@ RSpec.describe 'Task Requests' do
     end
 
     it 'should return a 403 if a user tries to access another user\'s tasks' do
-      allow_any_instance_of(Api::V1::ApplicationController).to receive(:current_user).and_return(@user1)
       get "/api/v1/users/#{@user2.id}/tasks", headers: { 'Authorization': "Bearer #{@user1.token}" }
+
+      expect(response).to have_http_status(403)
+    end
+
+    it 'should return 403 if the Authorization header is empty or not provided' do
+      get "/api/v1/users/#{@user2.id}/tasks", headers: { 'Authorization': "Bearer " }
+
+      expect(response).to have_http_status(403)
+
+      get "/api/v1/users/#{@user2.id}/tasks"
 
       expect(response).to have_http_status(403)
     end
